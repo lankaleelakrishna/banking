4cddcbd6b7da07a501da749deac649765a77dbc4
lankaleelakrishna <krishnalank004@gmail.com>
2024-12-23 22:40:33 +0530

    ii

--- banking/settings.py
+++ banking/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[
-    BASE_DIR/'static'
+    BASE_DIR/'static',
 ]
 
 
